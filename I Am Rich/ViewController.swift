af30adcad84fa7aeaeab04fef4dfe3390ae84e5f
Loay Naser <loai.alaa7@gmail.com>
2019-06-21 04:50:26 +0200

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Loai Alaa on 6/19/19.
+//  Copyright © 2019 Loai Alaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
